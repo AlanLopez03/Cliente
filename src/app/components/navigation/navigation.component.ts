0b0b8eb1dda41bca959f592ec79730b13533b185
Alan Lopez <lora030325@gs.utm.mx>
2024-04-01 00:40:53 -0600

    Se unio la rama de Saul

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -79,6 +79,8 @@ constructor(private router:Router,private categoriaService:CategoriaService,priv
 
   MostrarOfertas(id:any)
   {
+    //Debe redirigir al home
+    
     this.inventarioService.verOfertas(id);
 
   }
