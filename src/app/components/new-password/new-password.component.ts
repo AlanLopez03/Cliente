0f5aee5c9439001f20a129bd8845ccdeffb5be72
saulpena636 <119962073+saulpena636@users.noreply.github.com>
2024-04-01 22:59:29 -0600

    Add files via upload

--- src/app/components/new-password/new-password.component.ts
+++ src/app/components/new-password/new-password.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { UsuarioService } from '../../services/usuario/usuario.service';
 import { Router } from '@angular/router';
 import { UsuarioPassword } from '../../models/Usuario';
+import { ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2'
 
 @Component({
@@ -9,16 +10,50 @@ import Swal from 'sweetalert2'
   templateUrl: './new-password.component.html',
   styleUrl: './new-password.component.css'
 })
-export class NewPasswordComponent {
+export class NewPasswordComponent implements OnInit{
+  token : string = "";
 
   usuario: UsuarioPassword = new UsuarioPassword();
-  constructor(private usuarioService: UsuarioService, private router: Router) { }
+  constructor(private usuarioService: UsuarioService, private router: Router, private route: ActivatedRoute,) { }
 
   enviarPassword() {
-  
-      if (this.usuario.password == this.usuario.password1)
-        alert('Contraseña restablecida');
-      else
-        alert('Contraseña incorrecta');
+    if(this.usuario.password == "" || this.usuario.password1 ==""){
+      Swal.fire({
+        title: 'Error',
+        text: 'Por favor, asegurese de haber llenado todos los campos',
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      })
+      return;
+    }
+    else{
+      if (this.usuario.password != this.usuario.password1){
+        Swal.fire({
+          title: 'Error',
+          text: 'Las contraseñas no coinciden',
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        })
+        return;
+      }
+      else{
+        this.usuarioService.actualizarPassword(this.token, this.usuario.password).subscribe((res : any) => {
+          console.log(res);
+          Swal.fire({
+            title: 'Actualización exitosa',
+            text: 'Se ha actualizado su contraseña',
+            icon: 'success',
+            confirmButtonText: 'Aceptar'
+          })
+          this.router.navigateByUrl("")
+        }, err => console.error(err));
+      }
+    }
+  }
+  ngOnInit():void{
+    this.route.params.subscribe(params => {
+      this.token = params['token'];
+      console.log(this.token);
+    });
   }
 }
