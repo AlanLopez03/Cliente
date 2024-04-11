fdcba6819a3102cf42570ce8ada04b6bfb412adb
Alan Lopez <lora030325@gs.utm.mx>
2024-04-10 19:44:59 -0600

    Cliente local

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: true,
-    API_URI: "https://joyeria.onrender.com/api",
+    //API_URI: "https://joyeria.onrender.com/api",
+    API_URI: "http://localhost:3000/api",
+    API_URI_CORREOS: "http://localhost:3001",
     //API_URI: "http://localhost:3000/api",
-    API_URI_CORREOS: "https://servercorreos.onrender.com",
+    //API_URI_CORREOS: "https://servercorreos.onrender.com",
 }
