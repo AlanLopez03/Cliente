ee42bd618b54d4895b3c72e1c6394933bb87cc15
saulpena636 <119962073+saulpena636@users.noreply.github.com>
2024-04-14 11:38:36 -0600

    Primera version de traduccion dinamica

--- src/app/models/categoria.ts
+++ src/app/models/categoria.ts
@@ -1,10 +1,12 @@
 export class Categoria {
     idCategoria: number;
     nombre: string;
+    name :string;
 
     constructor() {
         this.idCategoria = 1;
         this.nombre = "";
+        this.name = "";
 
     }
     set(idCategoria: number) {
