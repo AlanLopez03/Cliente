694404dad1e8b830ae3f2596fc0b54729bc46cf2
saulpena636 <119962073+saulpena636@users.noreply.github.com>
2024-04-14 23:16:32 -0600

    Version acrualizada de la traduccion dinamica

--- src/app/models/material.ts
+++ src/app/models/material.ts
@@ -1,9 +1,11 @@
 export class Material {
     idMaterial: number;
     nombre: string;
+    name: string;
     constructor(){
         this.idMaterial = 1;
         this.nombre = '';
+        this.name='';
     }
     set(idMaterial: number) {
         this.idMaterial = idMaterial;
